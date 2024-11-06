3fa9fec478f5489d493edcaf5fcb177ec83775e5
Daria Ruzanova <kaiser7lu@gmail.com>
2024-11-06 20:58:47 +0600

    fix: fixed models.py

--- models.py
+++ models.py
@@ -1,19 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
+
 
 db = SQLAlchemy()
 
+
 class Card(db.Model):
-    __tablename__ = 'cards'
-    
+    __tablename__ = "cards"
+
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.Text, nullable=False)
     answer = db.Column(db.Text, nullable=False)
     card_type = db.Column(db.String(50))
     tags = db.Column(db.String(200))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     last_reviewed = db.Column(db.DateTime)
-    next_review = db.Column(db.DateTime, default=datetime.utcnow)
+    next_review = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     review_count = db.Column(db.Integer, default=0)
     easiness_factor = db.Column(db.Float, default=2.5)
     interval = db.Column(db.Integer, default=1)
